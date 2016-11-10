6fbe268e7800e75c21b1d37887d0044f27e82f07
Wulan Rohmadani <wulanr161299@gmail.com>
2016-11-10 20:51:08 +0700

    Initial commit SplashScreen Done

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl406152433/mj1/SplashScreen.java
@@ -0,0 +1,40 @@
+package id.sch.smktelkom_mlg.project.xirpl406152433.mj1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Milo Nestlea on 11/10/2016.
+ */
+
+public class SplashScreen extends Activity {
+
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                this.finish();
+            }
+
+            private void finish() {
+
+            }
+        }, splashInterval);
+    }
+}
